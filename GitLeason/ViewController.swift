408bed75ee47a3a85bcd53fbd0a498f3ddc09251
Kevin He <3057469318@qq.com>
2017-12-30 16:31:28 +0800

    Revert

--- GitLeason/ViewController.swift
+++ GitLeason/ViewController.swift
@@ -12,9 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello CS193p")
-        print("Hello World!")
+        // Do any additional setup after loading the view, typically from a nib.
     }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
 
 }
 
